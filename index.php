3c29312ac3b31562a5b49174de2d1bae20ace7a2
Jorge Rodrigues <jorgerodrigues9@outlook.com>
2018-07-13 20:21:53 -0400

    Teste.

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
                     </g>
                 </svg>
                     <h1 class="pfeatured__box--line">
-                        Developer team <span class="w">|</span>
+                        TESTE Developer team <span class="w">|</span>
                     </h1>
                 </div>
             </div>
